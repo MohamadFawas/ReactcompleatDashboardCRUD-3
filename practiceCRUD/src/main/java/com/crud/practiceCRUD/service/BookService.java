bb70892a90229672e2e79c4468dd19fad61966f7
MohamadFawas <mohamadfawas19971003@gmail.com>
2023-08-04 09:25:33 +0530

    backend code

---
+++ practiceCRUD/src/main/java/com/crud/practiceCRUD/service/BookService.java
@@ -0,0 +1,26 @@
+package com.crud.practiceCRUD.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.crud.practiceCRUD.requestdto.BookRequestDto;
+import com.crud.practiceCRUD.responce.BookResponceDto;
+
+public interface BookService {
+	public void saveLibrary_management(BookRequestDto bookRequestDto ); 
+	public List<BookResponceDto> getAllLibrary_management();
+	
+	public boolean existsIndexNumberIgnoreCase(String indexNumber);
+	public boolean existByLibrary_management(Long id);
+	
+	public BookResponceDto getLibrary_managementById(Long id);
+	
+	//public boolean getLibrary_managementById(Long id);
+	public void deleteLibrary_management(Long id);
+	
+//	void importProjectStatuss(MultipartFile file) throws IOException;
+	
+	public boolean importProjectStatusesFromCsv(MultipartFile file) ;
+}
